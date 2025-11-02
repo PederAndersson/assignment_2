a90099bd5ca45ddc96a331892fa51918c762f792
Peder Andersson <peder.andersson@live.se>
2025-11-02 20:31:58 +0100

    minor code refractoring

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iomanip>
 #include <iostream>
-#include <windows.h>
 #include "sensor.h"
 #include "DataGenerator.h"
 #include "measurement.h"
 
 int main() {
-SetConsoleOutputCP(CP_UTF8);
+
     std::cout << std::fixed << std::setprecision(2);
     DataGenerator gen;
     Measurement data;
     auto temp1 = SensorFactory::generateTempdata(gen,1);
     auto humid1 = SensorFactory::generateHumiditydata(gen,1);
     auto noise1 = SensorFactory::generateNoisedata(gen,1);
+
+    data.addMeasurement(temp1,humid1,noise1);
+
 
     data.printAll();
 
