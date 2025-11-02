91b8a3b53274e736c4803ffd475b6b1d4d7411fd
Peder Andersson <peder.andersson@live.se>
2025-11-02 10:53:47 +0100

    added a struct with a vector for each sensor and a vector to hold the struct.

--- measurement.h
+++ measurement.h
@@ -1,12 +1,18 @@
-//
-// Created by peder on 2025-10-30.
-//
-
 #ifndef ASSIGNMENT_2_MEASUREMENT_H
 #define ASSIGNMENT_2_MEASUREMENT_H
 
+#include <vector>
+#include "sensor.h"
+
+struct SensorHub {
+    std::vector<TempSensor> temp;
+    std::vector<HumiditySensor> humsens;
+    std::vector<AirqualitySensor> Airsens;
+};
 
 class measurement {
+private:
+    std::vector<SensorHub> hub;
 };
 
 
