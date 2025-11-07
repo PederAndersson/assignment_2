20866aa59f657f9fdae5454226ea96487876f7a7
Peder Andersson <peder.andersson@live.se>
2025-11-07 19:22:49 +0100

    fixed a small bug

--- main.cpp
+++ main.cpp
@@ -62,6 +62,7 @@ int main() {
                         break;
                     }
                 }
+                break;
             }
             case 5: {
                 std::cout << "Clears the csv file.\n";
