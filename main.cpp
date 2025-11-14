0101e2e865818fa8f764fc2dab101db0a1d0336d
Peder Andersson <peder.andersson@live.se>
2025-11-14 17:28:41 +0100

    start working on a threshold detector

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 
-#include <filesystem>
 #include "src/measurement.h"
 #include "src/SystemController.h"
-namespace fs = std::filesystem;
 int main() {
     MeasurementStorage storage;
     SystemController controller(storage);
